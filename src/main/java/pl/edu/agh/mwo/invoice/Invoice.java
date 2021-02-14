9a18648353fa90a955869c47ac52287ca59cb889
Szymon <sbatko@student.agh.edu.pl>
2021-02-14 22:52:58 +0100

    Wszystkie testy na zielono

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -9,14 +9,19 @@ import java.util.Map;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Map<Product,Integer>products = new LinkedHashMap<>();
+    private Map<Product,Integer>products = new LinkedHashMap<Product, Integer>();
 
     public void addProduct(Product product) {
         this.products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
+    	if (quantity == 0 || quantity <0) {
+    		throw new IllegalArgumentException("Brak możliwosci dodania zerowej/ujemnej ilosci");
+    	}
+    	
     	this.products.put(product, quantity);
+
     }
 
     public BigDecimal getNetPrice() {
@@ -29,10 +34,21 @@ public class Invoice {
     }
 
     public BigDecimal getTax() {
-        return BigDecimal.ZERO;
+    	BigDecimal sum = BigDecimal.ZERO;
+    	for (Product product : this.products.keySet()) {
+    			Integer quantity = this.products.get(product);
+    			sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)).multiply(product.getTaxPercent()));
+    	}
+        return sum;
     }
+    
 
     public BigDecimal getTotal() {
-        return BigDecimal.ZERO;
+        BigDecimal sum = BigDecimal.ZERO;
+    	for (Product product : this.products.keySet()) {
+    			Integer quantity = this.products.get(product);
+    			sum = sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+    	}
+        return sum;
     }
 }
